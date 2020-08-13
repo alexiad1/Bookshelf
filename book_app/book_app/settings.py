e06b669d0d793ca2f4e0ce2073b8299d6f7a4f00
Alexis Diaz <alexisdiaz@Alexiss-MacBook-Pro-2.local>
2020-08-12 21:43:41 -0700

    staged some files that were not staged in last commit

--- book_app/book_app/settings.py
+++ book_app/book_app/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello_world',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +54,7 @@ ROOT_URLCONF = 'book_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['book_app/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
